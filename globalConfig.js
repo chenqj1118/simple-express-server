e0e676a5761fc1feb64519ce3628a96135ac1e4e
chenqj <chenqj1118@gmail.com>
2021-01-08 11:18:35 +0800

    简易express服务

---
+++ globalConfig.js
@@ -0,0 +1,3 @@
+module.exports = {
+    rootPath: process.cwd() + '\\userData\\' // 此目录将存放所有用户的数据，上线部署时 可自定义服务器上任意适合的目录，默认在当前进程的userData目录下
+}
---
+++ main.js
@@ -0,0 +1,33 @@
+/*读写本地文件*/
+const express = require("express");
+const app = express();
+const bodyParser = require('body-parser');
+const history = require('connect-history-api-fallback');
+const router = require('./router/index')  //  引入路由
+
+const globalConfig = require('./globalConfig');
+const utilts = require('./router/utils');
+const fs = require('fs');
+!fs.existsSync(globalConfig.rootPath) && utilts.mkdirsSync(globalConfig.rootPath); // 如果rootPath目录不存在，则创建目录
+
+// body-parser application/json解析 extended:false 不使用第三方模块处理参数，使用Nodejs内置模块querystring处理
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json()) //application/x-www-form-urlencoded解析
+app.use(history());
+
+//设置跨域访问
+app.all('*', (req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
+    res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By", ' 3.2.1');
+    res.header("Content-Type", "application/json;charset=utf-8");
+    if(req.method=="OPTIONS") res.send(200);/*让options请求快速返回*/
+    else  next();
+});
+
+//  使用路由 /index 是路由指向名称
+app.use("/router", router);
+let port = 8090;
+app.listen(port);
+console.info('server start successful!\nport is '+port);
